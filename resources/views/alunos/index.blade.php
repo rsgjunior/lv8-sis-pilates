186fc3d2591de814fac294806df32e83d4d57308
Ronaldo Júnior <ronaldoguimar@live.com>
2021-05-21 00:52:00 -0300

    Update Index Aluno

--- resources/views/alunos/index.blade.php
+++ resources/views/alunos/index.blade.php
@@ -12,102 +12,92 @@
 @stop
 
 @section('content')
-
-<div class="row">
-    <div class="col-12">
-      <div class="card">
-        <div class="card-header">
-          <a class="btn btn-primary" href="{{ route('alunos.create') }}">
-            <i class="fa fa-plus-circle" aria-hidden="true"></i>
-            Criar novo
-          </a>
-          <div class="card-tools">
-            <form action="{{ route('alunos.index') }}" method="GET">
-              <div class="input-group input-group-sm" style="width: 250px;">
-                <input type="text" name="pesquisa" style="height: 50px;" class="form-control float-right" placeholder="Procurar aluno">
-                <div class="input-group-append">
-                  <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
+<div class="card card-solid">
+  <div class="card-body pb-0">
+    <div class="row d-flex align-items-stretch">
+      @foreach ($alunos as $aluno)
+        {{-- Bloco Aluno Inicio --}}
+        <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
+          <div class="card bg-light">
+            <div class="card-header text-muted border-bottom-0">
+              {{ $aluno->profissao }}
+            </div>
+            <div class="card-body pt-0">
+              <div class="row">
+                <div class="col-7">
+                  <h2 class="lead"><b>{{ $aluno->nome }}</b></h2>
+                  <ul class="ml-4 mb-0 fa-ul text-muted">
+                    <li class="small mb-2">
+                      <span class="fa-li"><i class="fas fa-lg fa-map-marker-alt"></i></span> 
+                      {{ $aluno->endereco_rua }}, {{ $aluno->endereco_numero }} 
+                      - {{ $aluno->endereco_cidade }}/{{ $aluno->endereco_estado }}
+                    </li>
+                    <li class="small mb-2">
+                      <span class="fa-li"><i class="fas fa-lg fa-birthday-cake"></i></span> 
+                      {{ date('d/m/Y', strtotime($aluno->data_nascimento))}} ({{ $aluno->idade }} anos)
+                    </li>
+                    <li class="small mb-2">
+                      <span class="fa-li"><i class="fas fa-lg fa-envelope"></i></span> 
+                      {{ $aluno->email }}
+                    </li>
+                    <li class="small mb-2">
+                      <span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> 
+                      {{ $aluno->telefone }}
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-5 text-center">
+                  <img src="{{ $aluno->foto_url }}" alt="" class="img-circle img-fluid">
                 </div>
               </div>
-            </form>
-            
+            </div>
+            <div class="card-footer">
+              <div class="text-right">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+                    <i class="fas fa-lg fa-user-cog"></i>
+                  </button>
+                  <ul class="dropdown-menu" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(-10px, 38px, 0px);">
+                    <li>
+                      <a href="{{ route('alunos.edit', ['aluno' => $aluno->id]) }}" class="dropdown-item">
+                        <i class="fa fa-edit" aria-hidden="true"></i>
+                          Editar        
+                      </a>
+                    </li>
+                    <li>
+                      <form action="{{ route('alunos.destroy', ['aluno' => $aluno->id]) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                          <button type="submit" class="dropdown-item">
+                            <i class="fa fa-trash" aria-hidden="true"></i> 
+                            Excluir        
+                          </button>
+                      </form>
+                    </li>
+                  </ul>
+                </div>
+
+                <a href="{{ route('alunos.show', $aluno) }}" class="btn btn-sm btn-primary">
+                  <i class="fas fa-user"></i> Ver Perfil
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-        <!-- /.card-header -->
-        <div class="card-body table-responsive p-0">
-          <table class="table table-hover" id="tabela">
-            <thead>
-              <tr>
-                <th>Foto</th>
-                <th>Nome</th>
-                <th>E-mail</th>
-                <th>Telefone</th>
-                <th>Data de Nascimento</th>
-                <th>Ações</th>
-              </tr>
-            </thead>
-            <tbody>
-            @foreach ($alunos as $aluno)
-            <tr>
-                <td>
-                  <img 
-                    class="table-img"
-                    @if($aluno->foto)
-                      src="{{ url('/storage/fotos/'. $aluno->id . '/' . $aluno->foto) }}" 
-                    @else
-                      src="{{ url('/img/default.jpg') }}" 
-                    @endif
-                  >
-                </td>
-                <td><a href="{{ route('alunos.show', ['aluno' => $aluno->id]) }}">{{ $aluno->nome }}</a></td>
-                <td>{{ $aluno->email }}</td>
-                <td>{{ $aluno->telefone }}</td>
-                <td>{{ date('d/m/Y', strtotime($aluno->data_nascimento)) }}</td>
-                <td>
-                  <a href="{{ route('alunos.show', ['aluno' => $aluno->id]) }}" class="d-inline-block btn btn-primary btn-sm">
-                    <i class="fa fa-eye" aria-hidden="true"></i>
-                    Ver        
-                  </a>
-  
-                  <a href="{{ route('alunos.edit', ['aluno' => $aluno->id]) }}" class="d-inline-block btn btn-info btn-sm">
-                    <i class="fa fa-edit" aria-hidden="true"></i>
-                      Editar        
-                  </a>
-                  <form class="d-inline-block" action="{{ route('alunos.destroy', ['aluno' => $aluno->id]) }}" method="post">
-                  @csrf
-                  @method('DELETE')
-                    <button type="submit" class="btn btn-danger btn-sm">
-                      <i class="fa fa-trash" aria-hidden="true"></i>
-                      Excluir        
-                    </button>
-                  </form>
-                </td>
-            </tr>
-            @endforeach
-              
-            </tbody>
-          </table>
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-          {{ $alunos->links() }}
-        </div>
-        
-      </div>
-      <!-- /.card -->
+        {{-- Bloco Aluno Fim --}}
+      @endforeach
+
     </div>
   </div>
-
+  <!-- /.card-body -->
+  <div class="card-footer">
+    {{ $alunos->links() }}
+  </div>
+  <!-- /.card-footer -->
+</div>
 @stop
 
 @section('css')
-<style>
-.table-img {
-  width: 2.5rem;
-  height: 2.5rem;
-  border-radius: 99999px;
-}
-</style>
 @stop
 
 @section('js')
